ccb3525a8bbc7952655957a0bd7705cda94650e0
Ben <ben.browne@gmail.com>
2016-10-13 21:50:42 -0700

    working feature extractor using itertools.groupby and absract base classes

--- parse_torque_temp.py
+++ parse_torque_temp.py
@@ -1,3 +1,4 @@
+
 """
 - reads from stdin
 - 'breaks the stream' when device or user defined time interval changes
@@ -11,55 +12,73 @@ data format:
  0	1420156780	drive_unit	{"torque": -0.08595670646206277, "temperature": 1.553783619625692},
  0	1420156790	drive_unit	{"torque": -0.9478755887865989, "temperature": 0.5052556076573897}]
 """
-
-import sys
-import json
-import time
-import pandas as pd
-import features
+import abc
 import argparse
+import itertools
+import json
+import sys
+import time
+
+import pandas as pd
+
+import features
 
 
-def parse_stdin(stdin):
-    """generator of a data dictionary from each string of raw data"""
-    for line in stdin:
-        device_id, epoch_time, data_type, json_data = line.strip('\n').split('\t')
-        epoch_time = int(epoch_time)
-        json_data = json.loads(json_data)
-        torque = json_data['torque']
-        temperature = json_data['temperature']
-        yield ({'device_id': device_id, 'epoch_time': epoch_time, 'data_type': data_type, 'torque': torque,
-                'temperature': temperature})  # how to do this more efficiently?
+class BaseFeatureExtractor(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, interval_type, out_file):
+        self.interval_type = interval_type
+        self.out_file = out_file
+
+    @abc.abstractmethod
+    def get_deviceid(self, line):
+        pass
+
+    @abc.abstractmethod
+    def get_interval(self, line):
+        pass
+
+    @abc.abstractmethod
+    def parse_raw_stream(self, raw_data):
+        """generator of dictionary from each string of raw data"""
+        pass
+
+    def process_parsed_stream(self, parsed_stream):
+        """Takes iterator of data dictionaries. Returns features, one line per time interval"""
+        data_block = pd.DataFrame()
+        for device_id, device_stream in itertools.groupby(parsed_stream, self.get_deviceid):
+            for interval, interval_stream in itertools.groupby(device_stream, self.get_interval):
+                for line in interval_stream:
+                    data_block = data_block.append(pd.DataFrame.from_records([line]))
+                line_of_output = {'device_id': device_id, 'interval': interval}
+                line_of_output.update({key: features.feature_input[key](data_block) for key in features.feature_input})
+                print >> outfile, line_of_output
 
 
-def main(stdin, out_file=sys.stdout, interval='hour'):
-    """prints reduced features from raw data by sending dictionaries from parse_stdin to the 'features' module"""
-    # using these to check for new hours or devices
-    previous_interval = previous_device_id = 'none'
+class CarFeatureExtractor(BaseFeatureExtractor):
 
-    # start reading the data
-    for index, line_of_data in enumerate(parse_stdin(stdin)):
+    def parse_raw_stream(self, raw_data):
+        """generator of a data dictionary from each string of raw data"""
+        for line in raw_data:
+            device_id, epoch_time, data_type, json_data = line.strip('\n').split('\t')
+            epoch_time = int(epoch_time)
+            json_data = json.loads(json_data)
+            torque = json_data['torque']
+            temperature = json_data['temperature']
+            yield ({'device_id': device_id, 'epoch_time': epoch_time, 'data_type': data_type, 'torque': torque,
+                    'temperature': temperature})
 
-        time_obj = time.localtime(line_of_data['epoch_time'])
-        if interval == 'hour':
-            current_interval = time_obj.tm_hour
-        elif interval == 'day':
-            current_interval = time_obj.tm_yday
+    def get_deviceid(self, line):
+        return line['device_id']
 
-        device_id = line_of_data['device_id']
-        # check if hour/day or device have changed and if so return features and clear the dataframe
-        if previous_interval != current_interval or previous_device_id != line_of_data['device_id']:
-            if previous_interval != 'none':
-                line_of_output = {interval: current_interval, 'device_id': device_id}
-                line_of_output.update({key: features.feature_input[key](df) for key in features.feature_input})
-                print >> out_file, line_of_output
-            df = pd.DataFrame()
-        else:
-            df = df.append(pd.DataFrame(line_of_data, index=[index]))
+    def get_interval(self, line):
+        time_obj = time.localtime(line['epoch_time'])
+        if self.interval_type == 'hour':
+            return time_obj.tm_hour
+        elif self.interval_type == 'day':
+            return time_obj.tm_yday
 
-        # note the time and device for comparison with the next iteration
-        previous_device_id = device_id
-        previous_interval = current_interval
 
 def parse_cl_args():
     parser = argparse.ArgumentParser(description='print dictionary of features from stream of torque and temperature data')
@@ -67,4 +86,5 @@ def parse_cl_args():
     return parser.parse_args(sys.argv[1:]).interval
 
 if __name__ == '__main__':
-    main(sys.stdin, interval=parse_cl_args())
+    feature_extractor = CarFeatureExtractor(interval_type=parse_cl_args(), out_file=sys.stdout)
+    feature_extractor.process_parsed_stream(feature_extractor.parse_raw_stream(sys.stdin))
