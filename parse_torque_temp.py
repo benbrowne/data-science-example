0eda91b70dfe1913f0c5c676b0b9e274f69b83b3
Ben <ben.browne@gmail.com>
2016-10-24 21:57:30 -0700

    username and password as args. Event time to mysql

--- parse_torque_temp.py
+++ parse_torque_temp.py
@@ -29,15 +29,36 @@ class BaseFeatureExtractor(object):
 
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, interval_type='hour'):
+    def __init__(self, interval_type):
         self.interval_type = interval_type
+        self.features = None
+
+    def get_features(self, stream):
+        self.features = [feature for feature in self.process_parsed_stream(self.parse_raw_stream(stream))]
+
+    def publish(self, username=None, password=None, target='stdout'):
+        if target == 'stdout':
+            print >> sys.stdout, self.features
+        elif target == 'mysql':
+            if args.username is None or args.password is None:
+                print >> sys.stdout, self.features
+                print >> sys.stdout, 'Did not write to mysql. Enter mysql username and password arguments to parse_torque_temp'
+            else:
+                con = pymysql.connect(user=username, password=password, database='features')
+                cur = con.cursor()
+                for line in self.features:
+                    print line
+                    cur.execute("INSERT INTO hourly VALUES (null, %s, %s, %s, %s)",
+                                (line['device_id'], time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(line['epoch_time'])),
+                                 self.feature_class, str(line)))
+                con.commit()
 
     @abc.abstractproperty
     def feature_class(self):
         pass
 
     @abc.abstractmethod
-    def get_deviceid(self, line):
+    def get_device_id(self, line):
         pass
 
     @abc.abstractmethod
@@ -52,28 +73,29 @@ class BaseFeatureExtractor(object):
     def process_parsed_stream(self, parsed_stream):
         """Takes iterator of data dictionaries. Returns features, one line per time interval"""
         data_block = pd.DataFrame()
-        for device_id, device_stream in itertools.groupby(parsed_stream, self.get_deviceid):
+        for device_id, device_stream in itertools.groupby(parsed_stream, self.get_device_id):
             for interval, interval_stream in itertools.groupby(device_stream, self.get_interval):
                 for line in interval_stream:
                     data_block = data_block.append(pd.DataFrame.from_records([line]))
-                line_of_output = {'device_id': device_id, 'interval': interval}
+                line_of_output = {'device_id': device_id, 'interval': interval, 'epoch_time': data_block['epoch_time'].max()}
                 line_of_output.update({key: features.feature_input[key](data_block) for key in features.feature_input})
                 yield line_of_output
 
-    def features_to_mysql(self, features):
-        con = pymysql.connect(user='root', password='picanha', database='features')
-        cur = con.cursor()
-        for line in features:
-            cur.execute("INSERT INTO hourly VALUES (null, %i, %i, %s, %s)", (line['device_id'], line['interval'], self.feature_class, line))
 
 class CarFeatureExtractor(BaseFeatureExtractor):
 
-    self.feature_class = 'car'
+    def __init__(self, interval_type='hour'):
+        BaseFeatureExtractor.__init__(self, interval_type)
+        self.feature_class()
+
+    def feature_class(self):
+        self.feature_class = 'car'
 
     def parse_raw_stream(self, raw_data):
         """generator of a data dictionary from each string of raw data"""
         for line in raw_data:
             device_id, epoch_time, data_type, json_data = line.strip('\n').split('\t')
+            device_id = int(device_id)
             epoch_time = int(epoch_time)
             json_data = json.loads(json_data)
             torque = json_data['torque']
@@ -81,7 +103,7 @@ class CarFeatureExtractor(BaseFeatureExtractor):
             yield ({'device_id': device_id, 'epoch_time': epoch_time, 'data_type': data_type, 'torque': torque,
                     'temperature': temperature})
 
-    def get_deviceid(self, line):
+    def get_device_id(self, line):
         return line['device_id']
 
     def get_interval(self, line):
@@ -94,6 +116,8 @@ class CarFeatureExtractor(BaseFeatureExtractor):
 
 def parse_cl_args():
     parser = argparse.ArgumentParser(description='print dictionary of features from stream of torque and temperature data')
+    parser.add_argument('--username', '-u', help='mysql username', default=None)
+    parser.add_argument('--password', '-p', help='mysql password', default=None)
     parser.add_argument('--interval', '-i', choices=['hour', 'day'], default='hour')
     parser.add_argument('--to', '-t', choices=['mysql', 'stdout'], default='stdout')
     return parser.parse_args(sys.argv[1:])
@@ -101,8 +125,5 @@ def parse_cl_args():
 if __name__ == '__main__':
     args = parse_cl_args()
     feature_extractor = CarFeatureExtractor(interval_type=args.interval)
-    features = [feature for feature in feature_extractor.process_parsed_stream(feature_extractor.parse_raw_stream(sys.stdin))]
-        if args.to == 'stdout':
-            iprint >> sys.stdout, features
-        elif args.to == 'mysql':
-            features_to_mysql(features)
+    feature_extractor.get_features(sys.stdin)
+    feature_extractor.publish(username=args.username, password=args.password, target=args.to)
